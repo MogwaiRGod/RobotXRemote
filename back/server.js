08cc3b61efbb64ee69048487b66ed6c5eb5cd69d
Mogwai <diane.roberg@gmail.com>
2023-02-25 21:35:45 +0000

    premier commit via docker !

---
+++ back/server.js
@@ -0,0 +1,36 @@
+/**
+ * # README
+ * 
+ * ## FONCTIONNEMENT
+ * Application Express tournant sur un serveur HTTP lui-même monté sur un serveur socket.io, afin de bénéficier de ses connexions en temps-réel.
+ * 
+ * 
+ * ## CONTENU
+ * /back
+ * -> server.js
+ * L'application-serveur qui gère les requêtes envoyées du côté client (la télécommande) et qui en renvoie à un autre client (le robot)
+ * /views
+ * -> /html
+ * -> -> remote.html
+ * Boutons qui, au clic, permettent de déplacer le robot. Les clics envoient des requêtes au serveur.
+ * Fonctionne également à la pression de touches (flèches ou QZDS)
+ * -> -> robot.html
+ * 
+ */ 
+
+/**
+ * MODULES
+ */
+
+// notre application sera une application express
+const express = require('express');
+// on va la faire tourner sur un serveur HTTP si les navigateurs ne peuvent pas utiliser les connexions Web Socket de socket.io
+const http = require('http');
+// puis, on montera ce serveur sur un autre serveur, socket.io cette fois-ci
+const io = require('socket.io');
+
+// console.log("THEY CAME LIKE MOTHS TO A FLAAAAAAAAAAAAAAAAAAAAAAAAAME")
+// instanciation de l'app
+const APP = express();
+
+
